9b78d46b231c78e8ab91b7e86f6cf7aa022b0b22
kazinov <dm.kazinov@gmail.com>
2019-04-09 13:53:37 +0200

    fix

--- src/api/utils/is-body-valid.ts
+++ src/api/utils/is-body-valid.ts
@@ -1,7 +1,7 @@
 import { Request } from 'express-serve-static-core';
 
 export function isBodyValid(req: Request, requiredFields: string[]) {
-  for (let requiredParameter of ['name']) {
+  for (let requiredParameter of requiredFields) {
     if (!req.body[requiredParameter]) {
       return false;
     }
